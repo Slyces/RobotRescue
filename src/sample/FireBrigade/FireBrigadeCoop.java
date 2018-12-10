e8aefb2a9c7896b5bb88066432920868910ce4f5
Maxime Desbois <3875714@ppti-14-407-11.ufr-info-p6.jussieu.fr>
2018-12-10 18:21:23 +0100

    truc

--- src/sample/FireBrigade/FireBrigadeCoop.java
+++ src/sample/FireBrigade/FireBrigadeCoop.java
@@ -13,119 +13,114 @@ import sample.*;
 import static rescuecore2.misc.Handy.objectsToIDs;
 
 /**
-   A sample fire brigade agent.
+ * A sample fire brigade agent.
  */
 public class FireBrigadeCoop extends FireBrigadeDummy {
 
 	public static int ACTION_NUMBER = 6;
 
-    /* Méthodes pour le QLearning 'coop' */
+	/* Méthodes pour le QLearning 'coop' */
 
-    /*
-     * Espace des états :
-     *  - Présence d'un feu [bool]
-     *  - Niveau d'eau [bool]
-     *  - Team-Mate Nord [bool]
-     *  - Team-Mate Est [bool]
-     *  - Team-Mate Sud [bool]
-     *  - Team-Mate Ouest [bool]
-     *
-     * Espace des actions
-     *  - Move Nord
-     *  - Move Est
-     *  - Move Sud
-     *  - Move Ouest
-     *  - Respenish Water
-     *  - Extinguish Fire
-     *
-     * Taille de Q : 2**6 x 6
-     * */
+	/*
+	 * Espace des états : - Présence d'un feu [bool] - Niveau d'eau [bool] -
+	 * Team-Mate Nord [bool] - Team-Mate Est [bool] - Team-Mate Sud [bool] -
+	 * Team-Mate Ouest [bool]
+	 *
+	 * Espace des actions - Move Nord - Move Est - Move Sud - Move Ouest - Respenish
+	 * Water - Extinguish Fire
+	 *
+	 * Taille de Q : 2**6 x 6
+	 */
 
-    /* Etats */
-
+	/* Etats */
 
 	@Override
-    protected void postConnect() {
-        super.postConnect();
-        model.indexClass(StandardEntityURN.BUILDING, StandardEntityURN.REFUGE,StandardEntityURN.HYDRANT,StandardEntityURN.GAS_STATION);
-        maxWater = config.getIntValue(MAX_WATER_KEY);
-        maxDistance = config.getIntValue(MAX_DISTANCE_KEY);
-        maxPower = config.getIntValue(MAX_POWER_KEY);
-        Logger.info("Sample fire brigade connected: max extinguish distance = " + maxDistance + ", max power = " + maxPower + ", max tank = " + maxWater);
+	protected void postConnect() {
+		super.postConnect();
+		model.indexClass(StandardEntityURN.BUILDING, StandardEntityURN.REFUGE, StandardEntityURN.HYDRANT,
+				StandardEntityURN.GAS_STATION);
+		maxWater = config.getIntValue(MAX_WATER_KEY);
+		maxDistance = config.getIntValue(MAX_DISTANCE_KEY);
+		maxPower = config.getIntValue(MAX_POWER_KEY);
+		Logger.info("Sample fire brigade connected: max extinguish distance = " + maxDistance + ", max power = "
+				+ maxPower + ", max tank = " + maxWater);
 
-        this.me = me();
+		this.me = me();
 
-        /* Code d'initialisation */
-        Matrix m = Utils.loadCoop();
-        old_time = m.time;
-        Q = m.matrice;
+		/* Code d'initialisation */
+		Matrix m = Utils.loadCoop();
+		old_time = m.time;
+		Q = m.matrice;
 
-    }
+	}
 
 	private boolean waterLevel() {
-    	if (myWater == 0)
-        	return false;
-        else
-        	return true;
-    }
+		if (myWater == 0)
+			return false;
+		else
+			return true;
+	}
 
 	public QState getState() {
-        return new StateCoop(waterLevel(), isThereFire(), isThereFireBrigades());
-    }
+		return new StateCoop(waterLevel(), isThereFire(), isThereFireBrigades());
+	}
 
-    /* -------------- Move towards direction --------------- */
-    public double moveTowards(Direction direction, int time) {
-        Collection<StandardEntity> entities = model.getEntitiesOfType(StandardEntityURN.BUILDING);
-        Building best = null;
-        FireBrigade me = me();
-        double best_dist = 10e10;
-        for (StandardEntity e: entities) {
-            Building building = (Building) e;
-            if (Utils.direction(me, building) == direction) {
-                if (best == null || Utils.distance(me, building, direction) < best_dist) {
-                    best = building;
-                    best_dist = Utils.distance(me, building, direction);
-                }
-            }
-        }
-        // Try to get to anything within maxDistance of the target
-        EntityID target = best.getID();
-        Collection<StandardEntity> targets = model.getObjectsInRange(target, maxDistance);
-        List<EntityID> path = null;
-        if (! targets.isEmpty()) {
-            path = search.breadthFirstSearch(me().getPosition(), objectsToIDs(targets));
-        }
-        sendMove(time, path);
-        return 0;
-    }
+	/* -------------- Move towards direction --------------- */
+	public double moveTowards(Direction direction, int time) {
+		Collection<StandardEntity> entities = model.getEntitiesOfType(StandardEntityURN.BUILDING);
+		Building best = null;
+		FireBrigade me = me();
+		double best_dist = 10e10;
+		for (StandardEntity e : entities) {
+			Building building = (Building) e;
+			if (Utils.direction(me, building) == direction) {
+				if (best == null || Utils.distance(me, building, direction) < best_dist) {
+					best = building;
+					best_dist = Utils.distance(me, building, direction);
+				}
+			}
+		}
+		// Try to get to anything within maxDistance of the target
+		EntityID target = best.getID();
+		Collection<StandardEntity> targets = model.getObjectsInRange(target, maxDistance);
+		List<EntityID> path = null;
+		if (!targets.isEmpty()) {
+			path = search.breadthFirstSearch(me().getPosition(), objectsToIDs(targets));
+		}
+		sendMove(time, path);
+		return 0;
+	}
 
-    public boolean[] isThereFireBrigades() {
-    	List<Human> voisins = new ArrayList<Human>();
-    	boolean[] retour = new boolean[4];
-    	Collection<StandardEntity> e = model.getEntitiesOfType(StandardEntityURN.FIRE_BRIGADE);
-    	for (StandardEntity next : e) {
-    		Human h = (Human)next;
-            if (h == me()) {
-                continue;
-            }
-            voisins.add(h);
-    	}
-    	for (Human h : voisins) {
-    		if (Utils.direction(me(), (rescuecore2.standard.entities.FireBrigade)h) == Direction.NORTH ) {
-    			retour[0] = true;
-    		}
-    		if (Utils.direction(me(), (rescuecore2.standard.entities.FireBrigade)h) == Direction.EAST ) {
-    			retour[1] = true;
-    		}
-    		if (Utils.direction(me(), (rescuecore2.standard.entities.FireBrigade)h) == Direction.SOUTH ) {
-    			retour[2] = true;
-    		}
-    		if (Utils.direction(me(), (rescuecore2.standard.entities.FireBrigade)h) == Direction.WEST ) {
-    			retour[3] = true;
-    		}
-    	}
+	public boolean[] isThereFireBrigades() {
+		List<Human> voisins = new ArrayList<Human>();
+		boolean[] retour = new boolean[4];
+		Collection<StandardEntity> e = model.getEntitiesOfType(StandardEntityURN.FIRE_BRIGADE);
+		for (StandardEntity next : e) {
+			Human h = (Human) next;
+			if (h == me()) {
+				continue;
+			}
+			voisins.add(h);
+		}
+		for (Human h : voisins) {
+			if (model.getDistance(getID(), h.getID()) <= 10000) {
 
-        return retour;
-    }
+				if (Utils.direction(me(), (rescuecore2.standard.entities.FireBrigade) h) == Direction.NORTH) {
+					retour[0] = true;
+				}
+				if (Utils.direction(me(), (rescuecore2.standard.entities.FireBrigade) h) == Direction.EAST) {
+					retour[1] = true;
+				}
+				if (Utils.direction(me(), (rescuecore2.standard.entities.FireBrigade) h) == Direction.SOUTH) {
+					retour[2] = true;
+				}
+				if (Utils.direction(me(), (rescuecore2.standard.entities.FireBrigade) h) == Direction.WEST) {
+					retour[3] = true;
+				}
+			}
+		}
+
+		return retour;
+	}
 
 }
