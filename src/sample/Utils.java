3ad29b5a4cdd3656b14443ef8fe5dc571ef35f4e
Maxime Desbois <3875714@ppti-24-301-07.ufr-info-p6.jussieu.fr>
2018-12-10 14:05:52 +0100

    commit chelou

--- src/sample/Utils.java
+++ src/sample/Utils.java
@@ -50,8 +50,7 @@ public class Utils {
     
     public static void score(int time) {
     	if (time == 10) {
-    		double a = ScoreTable.ScoreModel.ScoreFunctionEntry.getScore(time);
-    		System.out.println("===============================================================" + a );
+    		System.out.println("===============================================================" );
     	}
     }
     
